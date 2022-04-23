f87a104ada8126a04b3ef53a660564107a89e543
Tahmeed Tarek <tahmeedtarek@gmail.com>
2022-04-23 20:49:58 +0600

    Setup connection
    - Setup `contractkit`, `web3` and basic usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Web3 from "web3";
+import { newKitFromWeb3 } from "@celo/contractkit";
+import { useContractKit, ContractKitProvider } from '@celo-tools/use-contractkit';
+import Elonium from './contracts/Elonium.json';
+
+import '@celo-tools/use-contractkit/lib/styles.css';
 import './App.css';
 
-function App() {
+function AppInside () {
+  const [symbol, setSymbol] = useState("-");
+  
+  const { address, connect } = useContractKit()
+  const web3 = new Web3("https://alfajores-forno.celo-testnet.org")
+  const kit = newKitFromWeb3(web3);
+  let elonium = new kit.connection.web3.eth.Contract(Elonium.abi, "0x996eb07440580cf1B28213ceeD3112ec86500bb8")
+
+  const init = async () => {
+    setSymbol(await elonium.methods.symbol().call());
+  }
+
+  init();
+
+  // Transactions with an unspecified feeCurrency field will default to paying fees in CELO
+  // const tx = await instance.methods.setName(newName).send({ from: account.address, feeCurrency: cUSDcontract.address })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Elonium</h3>
+        <p>{address}</p>
+        <p>Symbol: {symbol}</p>
+        <button onClick={connect}>Click here to connect your wallet</button>
       </header>
     </div>
+  )
+}
+
+function App() {
+  return (
+    <ContractKitProvider
+      dapp={{
+          name: "Elonium",
+          description: "An ERC 20 token deployed on Alfajores",
+          url: "",
+        }}
+    >
+      <AppInside />
+    </ContractKitProvider>
   );
 }
 
