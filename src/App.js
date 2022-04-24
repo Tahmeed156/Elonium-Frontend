7249f837f6be95e7e2253062dbfdf2513f48bed8
Tahmeed Tarek <tahmeedtarek@gmail.com>
2022-04-24 10:34:48 +0600

    Add basic info, owner features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
 import Web3 from "web3";
 import { newKitFromWeb3 } from "@celo/contractkit";
-import { useContractKit, ContractKitProvider } from '@celo-tools/use-contractkit';
+import { ContractKitProvider } from '@celo-tools/use-contractkit';
+import LoadingButton from '@mui/lab/LoadingButton';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
 import Elonium from './contracts/Elonium.json';
+import OwnerCard from './components/OwnerCard';
 
 import '@celo-tools/use-contractkit/lib/styles.css';
 import './App.css';
+import { Divider } from '@mui/material';
 
 function AppInside () {
-  const [symbol, setSymbol] = useState("-");
+  const [supply, setSupply] = useState(0);
+  const [balance, setBalance] = useState(0);
+  const [owner, setOwner] = useState(null);
+  const [loader, setLoader] = useState(false);
   
-  const { address, connect } = useContractKit()
+  // const { address, connect } = useContractKit()
   const web3 = new Web3("https://alfajores-forno.celo-testnet.org")
-  const kit = newKitFromWeb3(web3);
+  const kit = newKitFromWeb3(web3); 
+  const PRIVATE_KEY = '209e653bd3a9b0ddcb075164586b3a6dae7494a378a89895550849a2bbe52fed';
+  const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
+  kit.connection.addAccount(account.privateKey);
+  kit.defaultAccount = account.address;
   let elonium = new kit.connection.web3.eth.Contract(Elonium.abi, "0x996eb07440580cf1B28213ceeD3112ec86500bb8")
 
-  const init = async () => {
-    setSymbol(await elonium.methods.symbol().call());
+  async function init() {
+    setSupply(await elonium.methods.totalSupply().call());
+    setBalance(await elonium.methods.balanceOf(account.address).call());
+    setOwner(await elonium.methods.owner().call());
+    setLoader(false);
   }
 
   init();
@@ -27,10 +42,33 @@ function AppInside () {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Elonium</h3>
-        <p>{address}</p>
-        <p>Symbol: {symbol}</p>
-        <button onClick={connect}>Click here to connect your wallet</button>
+        <h1>Elonium (ELM)</h1>
+
+        <LoadingButton
+          size="small"
+          onClick={() => {
+            setLoader(true);
+            init();
+          }}
+          endIcon={<RefreshIcon />}
+          loading={loader}
+          loadingPosition="end"
+          variant="outlined"
+          style={{backgroundColor: "white", color: "black", border: 0}} 
+        >
+          Refresh
+        </LoadingButton>
+        <p>
+          Total supply: {supply / 1e18} ELM <br /> <br />
+          Account: {account.address} <br />
+          Balance: {balance / 1e18} ELM <br />
+        </p>
+        {/* {<Button variant="outlined" onClick={connect}>Connect wallet</Button>} */}
+
+        <Divider />
+
+        {owner !== null && owner === account.address && <OwnerCard elonium={elonium}/>}
+        {/* <button onClick={connect}>Click here to connect your wallet</button> */}
       </header>
     </div>
   )
---
+++ src/components/OwnerCard.js
@@ -0,0 +1,185 @@
+import React, { useState } from 'react';
+import { useContractKit } from '@celo-tools/use-contractkit';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function OwnerCard(el) {
+  const [tempDialog, setTempDialog] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState("");
+  const [mintDialog, setMintDialog] = useState(false);
+  const [transDialog, setTransDialog] = useState(false);
+  const [addressDialog, setAddressDialog] = useState(false);
+
+  const [mintValue, setMintValue] = useState(0);
+  const [transFromAddress, setTransFromAddress] = useState(null);
+  const [transToAddress, setTransToAddress] = useState(null);
+  const [transAmount, setTransAmount] = useState(0);
+  const [receiverAddress, setReceiverAddress] = useState(0);
+
+  const { address } = useContractKit();
+  const elonium = el.elonium;
+
+  async function transfer() {
+    if (transAmount <= 0)
+      return;
+    await elonium.methods.transfer(transToAddress, (transAmount * 1e18).toString()).send({ from: address, gas: 1e6 })
+      .then(setDialog)
+      .catch(setDialog);
+  }
+
+  function setDialog(obj) {
+    setDialogMessage(JSON.stringify(obj));
+    setTempDialog(true);
+  }
+
+  async function burnAllTokens() {
+    await elonium.methods.burnAllTokens().send({ from: address, gas: 1e6 })
+      .then(setDialog)
+      .catch(setDialog);
+  }
+
+  async function addReceiver() {
+    await elonium.methods.addReceiver(receiverAddress).send({ from: address, gas: 1e6 })
+      .then(setDialog)
+      .catch(setDialog);
+  }
+
+  async function removeReceiver() {
+    await elonium.methods.removeReceiver(receiverAddress).send({ from: address, gas: 1e6 })
+      .then(setDialog)
+      .catch(setDialog);
+  }
+
+  async function mintTokens() {
+    if (mintValue <= 0)
+      return;
+    await elonium.methods.mint(address, (mintValue * 1e18).toString()).send({ from: address, gas: 1e6 })
+      .then(setDialog)
+      .catch(setDialog);
+  }
+
+  return (
+    <Card sx={{ minWidth: 400 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          Exclusive Owner Features
+        </Typography>
+        <Button onClick={burnAllTokens} style={{ marginTop: "10px" }} size="small" variant="contained" color="error">BURN ALL TOKENS</Button> 
+        <br />
+        
+        <Button onClick={() => setMintDialog(true)} style={{ marginTop: "10px" }} size="small" variant="contained" color="success">MINT NEW TOKENS</Button>
+        <Dialog open={mintDialog} onClose={() => setMintDialog(false)}>
+          <DialogTitle>Mint New Tokens</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the amount of tokens to mint
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="mint-amount"
+              label="ELM tokens"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setMintValue(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {mintTokens(); setMintDialog(false)}} variant="outlined">Mint</Button>
+            <Button onClick={() => setMintDialog(false)}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+        <br />
+        
+        <Button onClick={() => setAddressDialog(true)} style={{ marginTop: "10px" }} size="small" variant="contained" color="success">MODIFY RECEIVERS</Button>
+        <Dialog open={addressDialog} onClose={() => setMintDialog(false)}>
+          <DialogTitle>Mint New Tokens</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the amount of tokens to mint
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="mint-amount"
+              label="Address"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setReceiverAddress(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {addReceiver(); setAddressDialog(false)}} variant="outlined">Add</Button>
+            <Button onClick={() => {removeReceiver(); setAddressDialog(false)}} variant="outlined">Remove</Button>
+            <Button onClick={() => setAddressDialog(false)}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+        <br />
+
+        <Button onClick={() => setTransDialog(true)} style={{ marginTop: "10px" }} size="small" variant="contained" color="success">TRANSFER TOKEN</Button>
+        <Dialog open={transDialog} onClose={() => setTransDialog(false)}>
+          <DialogTitle>Transfer Tokens</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the amount of tokens to transfer to a particular address
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="mint-amount"
+              label="Address"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setTransToAddress(e.target.value)}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="mint-amount"
+              label="ELM tokens"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setTransAmount(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {transfer(); setTransDialog(false)}} variant="outlined">Transfer</Button>
+            <Button onClick={() => setTransDialog(false)}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+
+        <Dialog
+          open={tempDialog}
+          scroll="paper"
+          onClose={() => setTempDialog(false)}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Trasaction result"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {dialogMessage}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setTempDialog(false)}>Ok</Button>
+          </DialogActions>
+        </Dialog>
+
+      </CardContent>
+      <CardActions>
+      </CardActions>
+    </Card>
+  );
+}
